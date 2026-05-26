8507c332df6837846640799e93940598612acb6f
W3lt <tienduy0123@gmail.com>
2026-05-26 21:22:19 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Create a new variable to store user input
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap_or_else(|err| {
+        eprintln!("Error: an unkown error occured: {err}");
+        exit(1);
+    });
+
+    input = input.trim().to_string();
+
+    println!("{input}: command not found")
 }
